651199099862e6806dcd8898277bdae2ae6e3055
Akashay Hosurkar <akashay.hosurkar@olacabs.com>
2022-07-20 16:41:42 +0530

    modified some components for each topic

---
+++ src/components/Home1.vue
@@ -0,0 +1,25 @@
+<template>
+<h1>Two Way Binding</h1>
+<input type="text" v-model="count" />
+<h2>{{count}}</h2>
+</template>
+
+<script>
+export default {
+    name: 'twoBinding',
+    data() {
+        return {
+            count:90
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1{
+    color: crimson;
+}
+h2{
+    color: red;
+}
+</style>
---
+++ src/components/Inputfield.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>Get Input Field value</h1>
+  <input type="text" placeholder="Enter Email" v-model="email"/>
+  <br/>
+  <br/>
+  <input type="text" placeholder="Enter Password" v-model="password"/>
+  <br/><br/>
+  <button v-on:click="getData()" type="button">Get Values</button>
+</template>
+
+<script>
+export default {
+name:'inputField',
+data(){
+return{
+    email:null,
+    password:null,
+}
+},
+methods:{
+    getData(){
+        console.warn("values:",this.email,this.password)
+    }
+}
+}
+</script>
+
+<style scoped>
+h1{
+    color:darkgreen
+}
+</style>
