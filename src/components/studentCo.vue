640ccd13676f8ba9b4d05c2e7366eb661dbf6276
Akashay Hosurkar <akashay.hosurkar@olacabs.com>
2022-07-26 16:06:44 +0530

    component is for props

---
+++ src/components/studentCo.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>Students Name is : {{name}}</h2>
+</template>
+
+<script>
+export default {
+name:'studentCo',
+props:{
+    name:String
+}
+}
+</script>
+
+<style scoped>
+h2{
+    color:black
+}
+</style>
