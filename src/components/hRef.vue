4b744a17d0f5ba6435c565376d291189b98fe6b0
Akashay Hosurkar <akashay.hosurkar@olacabs.com>
2022-07-26 16:04:52 +0530

    Ref is to get or set the value of Dom

---
+++ src/components/hRef.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <h1>Ref in Vue js 3</h1>
+    <input type="text" ref="input" />
+    <!--"input name could be anything" .is ref ke pass pure input field ka control aa gaya hai .whatever value u want  get or set osr style you can do it using ref-->
+    <button v-on:click="getData">CLICK ME!!!</button>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'hRef',
+    methods: {
+        getData() {
+            this.$refs.input.focus()
+            let val=this.$refs.input.value;
+            console.warn(val)
+            this.$refs.input.style.color="red"
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: cadetblue
+}
+</style>
