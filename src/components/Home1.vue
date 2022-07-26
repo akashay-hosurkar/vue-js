31a6f853bfeb6a09539127acf6fd63cc48dcc80a
Akashay Hosurkar <akashay.hosurkar@olacabs.com>
2022-07-26 15:57:24 +0530

    modified in Home1 component

--- src/components/Home1.vue
+++ src/components/Home1.vue
@@ -1,25 +1,37 @@
 <template>
-<h1>Two Way Binding</h1>
+<!-- <h1>Two Way Binding</h1>
 <input type="text" v-model="count" />
-<h2>{{count}}</h2>
+<h2>{{count}}</h2> -->
+<h1>Props in vue js</h1>
+<studentCo :name="name" />
+<teacherCo :name="name" />
 </template>
 
 <script>
+import studentCo from './studentCo.vue'
+import teacherCo from './teacherCo.vue'
+
 export default {
     name: 'twoBinding',
-    data() {
+    data() { // this data function is used for passing the same name
         return {
-            count:90
+            name: "THOR" //here could be anything array object, string,function,integer,number we can transfer anything as props
         }
+    },
+    components: {
+        studentCo,
+        teacherCo
     }
+    // data() {
+    //     return {
+    //         count:90
+    //     }
+    // }
 }
 </script>
 
 <style scoped>
-h1{
-    color: crimson;
+h1 {
+    color: orange
 }
-h2{
-    color: red;
-}
-
+</style>
